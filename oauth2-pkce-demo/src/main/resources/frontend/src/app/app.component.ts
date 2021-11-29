aba695bad1ac475aada9ec23b828dfdc70d00c4a
AKSHYA1997 <akshyaravishankar@hmail.com>
2021-11-29 21:44:08 +0530

    Front end oauth initial setup

--- oauth2-pkce-demo/src/main/resources/frontend/src/app/app.component.ts
+++ oauth2-pkce-demo/src/main/resources/frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {OAuthService} from "angular-oauth2-oidc";
+import {authConfig} from "./auth.config";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(private oauthService: OAuthService) {
+    this.configure();
+  }
+
+  login() {
+    this.oauthService.initCodeFlow();
+  }
+
+  private configure() {
+    this.oauthService.configure(authConfig);
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+
+  logout() {
+    this.oauthService.logOut();
+  }
 }
